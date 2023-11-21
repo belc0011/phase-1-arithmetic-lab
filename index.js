7f9f549351a2cb7991fd99fbb4c142fb65c81c61
Monica Garcia <monicairenegarcia@gmail.com>
2023-11-20 19:36:25 -0700

    completed arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 1;
+const num2 = 62/num1;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random()*10);
+
+const num3 = 36;
+const num4 = 8;
+
+let mod = num3%num4;
+
+let max = Math.max(1,2,5,20);
+
